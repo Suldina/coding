94e3e25e1400a7b2d050685d7a0d166fbdd3a8eb
Suldina <elenasuldina@mail.ru>
2017-08-20 23:15:55 +0300

    add structure of project

---
+++ Marmalade.java
@@ -0,0 +1,4 @@
+package com.company;
+
+public class Marmalade extends SweetPresent {
+}
---
+++ SweetPresent.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class SweetPresent {
+    public static void main(String[] args) {
+
+    }
+
+}
